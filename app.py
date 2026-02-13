00091d35f892deb28f795286641cd138a409614b
totosheyn-cyber <totosheyn@gmail.com>
2026-02-13 20:59:00 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # For local testing
+    app.run(host="0.0.0.0", port=10000, debug=True)
